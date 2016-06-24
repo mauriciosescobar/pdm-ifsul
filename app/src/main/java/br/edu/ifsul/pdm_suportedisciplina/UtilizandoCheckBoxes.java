b40fecd4e72bfaa5d699e0dfa6833cf03b6985b1
Mauricio Escobar <snkpit@gmail.com>
2016-06-24 15:25:02 -0300

    update

---
+++ app/src/main/java/br/edu/ifsul/pdm_suportedisciplina/UtilizandoCheckBoxes.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.pdm_suportedisciplina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UtilizandoCheckBoxes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utilizando_check_boxes);
+    }
+}
